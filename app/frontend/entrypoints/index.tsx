40be6bd8af8b34ebad19e327bc1db1c41afd08a3
Tou-u <rodrignacio99@hotmail.com>
2024-04-09 04:18:09 -0400

    Frontend setup

--- app/frontend/entrypoints/index.tsx
+++ app/frontend/entrypoints/index.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { NextUIProvider } from '@nextui-org/react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-import App from '../components/App';
+import App from '../components/App'
+import Comments from '../components/Comments'
 
-ReactDOM.render(
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/comments/:id',
+    element: <Comments />,
+  },
+])
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <NextUIProvider>
+      <main className='dark text-foreground bg-background'>
+        <RouterProvider router={router} />
+      </main>
+    </NextUIProvider>
+  </React.StrictMode>
 )
