fd517cc040da21c212a70bf42ee3fec01530d347
Tou-u <rodrignacio99@hotmail.com>
2024-04-09 04:26:19 -0400

    Routes for comments and refresh/update features

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root 'homepage#index'
+  get 'comments/:id', to: 'comments#index'
   namespace :api do
-    resources :features, only: [:index]
+    resources :features, only: [:index, :show] do
+      resources :comments, only: [:index, :create]
+    end
+    get 'refresh', to: 'features#refresh'
+    get 'up' => 'rails/health#show', as: :rails_health_check
   end
-  get 'up' => 'rails/health#show', as: :rails_health_check
 end
