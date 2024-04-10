243b2b463d0221658a163b32ee2173f27060c927
Tou-u <rodrignacio99@hotmail.com>
2024-04-09 23:06:16 -0400

    Pagination component

---
+++ app/frontend/components/Pagination.tsx
@@ -0,0 +1,21 @@
+import { Pagination as PaginationNU } from '@nextui-org/react'
+
+interface Props {
+  total: number
+  page: number
+  onChange: (page: number) => void
+}
+
+export default function Pagination({ total, page, onChange }: Props) {
+  return (
+    <PaginationNU
+      total={total}
+      initialPage={1}
+      page={total < page ? total : page}
+      color='secondary'
+      showControls
+      showShadow
+      onChange={onChange}
+    />
+  )
+}
