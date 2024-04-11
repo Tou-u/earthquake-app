5fdddf6786e692d44aa47d30bd7c42698044d1e3
Tou-u <rodrignacio99@hotmail.com>
2024-04-11 01:44:37 -0400

    Whitelist host

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the Rails application.
 require_relative "application"
 
+Rails.application.configure do
+  config.hosts << "earthquake.rodrigort.com"
+end
+
 # Initialize the Rails application.
 Rails.application.initialize!
